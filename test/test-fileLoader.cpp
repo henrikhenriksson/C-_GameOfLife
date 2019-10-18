313c89b2eafb01b7e67abad7965193938903708a
Henrik Henriksson <hehe0601@student.miun.se>
2019-10-18 09:19:41 +0200

    test-class-fileloader

--- test/test-fileLoader.cpp
+++ test/test-fileLoader.cpp
@@ -16,6 +16,7 @@
  * @details In this scenario the test is given a valid filename. The test then
  * makes sure the file is loaded and that a world is created based on the
  * dimensions specified in the file. (Height+2) * (Width+2).
+ * this test currently assumes the valid file is places in the same folder as the .exe.
  */
 SCENARIO("Loading and building a population from a valid file") {
   GIVEN("a Map holding cells") {
