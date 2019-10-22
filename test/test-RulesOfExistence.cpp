b51ce851f66b34920d339917afd7ad22231077da
johan <jkarlsson84@gmail.com>
2019-10-22 11:51:23 +0200

    Add test files for TulesOfExistance

---
+++ test/test-RulesOfExistence.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file test-RulesOfExistence.cpp
+ *
+ * @brief Contains test cases for the class RulesOfExistence
+ */
+#include "catch.hpp"
+#include "TestClassRulesOfExistence.hpp"
+
+SCENARIO("Set constructor values should be stored in class") {
+    GIVEN("Default constuctor used") {
+        //map<Point, Cell> cells;
+        //TestClassRulesOfExistence testClass(cells);
+        //testDefaultConstructor(testClass);
+
+        REQUIRE(false);
+    }
+}
