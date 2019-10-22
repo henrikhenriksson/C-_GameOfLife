a9cd43b375aa9bfc69ff99634f6ed5700c31a10e
Henrik Henriksson <hehe0601@student.miun.se>
2019-10-22 10:02:41 +0200

    update-doc-class-FileLoader. Updated documentation of the header and test files. added @bug found while testing.

--- test/test-fileLoader.cpp
+++ test/test-fileLoader.cpp
@@ -12,11 +12,12 @@
 #include "catch.hpp"
 //---------------------------------------------------------------------------
 /**
- * @brief test the loadPopulationFromFile
+ * @brief Test the loadPopulationFromFile
  * @details In this scenario the test is given a valid filename. The test then
  * makes sure the file is loaded and that a world is created based on the
  * dimensions specified in the file. (Height+2) * (Width+2).
- * this test currently assumes the valid file is places in the same folder as the .exe.
+ * this test currently assumes the valid file is places in the same folder as
+ * the .exe.
  */
 SCENARIO("Loading and building a population from a valid file") {
   GIVEN("a Map holding cells") {
@@ -32,7 +33,12 @@ SCENARIO("Loading and building a population from a valid file") {
   }
 }
 //---------------------------------------------------------------------------
-
+/**
+ * @brief Test invalid fileName
+ * @details In this scenarion the test is given an invalid filename. the
+ * function should then correctly throw an exception and return an error message
+ * to the user.
+ */
 SCENARIO("Loading a population from an invalid file") {
   GIVEN("a Map holding cells") {
     map<Point, Cell> cells;
