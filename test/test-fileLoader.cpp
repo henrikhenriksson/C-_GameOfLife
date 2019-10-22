6d376a42d1840bab0bec042633c4ad5fd47fc31b
Henrik Henriksson <hehe0601@student.miun.se>
2019-10-22 11:48:27 +0200

    update-doc-class-Fileloader. Found an issue with the global variable fileName as it interferes with subsequent testing in population.h. I moved the test with invalid fileName to the top to avoid this issue for the moment.

--- test/test-fileLoader.cpp
+++ test/test-fileLoader.cpp
@@ -12,6 +12,29 @@
 #include "catch.hpp"
 //---------------------------------------------------------------------------
 /**
+ * @brief Test invalid fileName
+ * @details In this scenarion the test is given an invalid filename. the
+ * function should then correctly throw an exception and return an error message
+ * to the user.
+ * @bug As the variable "fileName" is a global variable. This test creates
+ * issues in later tests as they will all print the error message "file not
+ * found, closing application".
+ */
+SCENARIO("Loading a population from an invalid file") {
+  GIVEN("a Map holding cells") {
+    map<Point, Cell> cells;
+    WHEN("an invalid filename is entered:") {
+      fileName = "invalid.txt";
+      FileLoader fileloader;
+      THEN("an exception should be thrown, with error message:") {
+        REQUIRE_THROWS_WITH(fileloader.loadPopulationFromFile(cells),
+                            "Could not find file. Closing application");
+      }
+    }
+  }
+}
+
+/**
  * @brief Test the loadPopulationFromFile
  * @details In this scenario the test is given a valid filename. The test then
  * makes sure the file is loaded and that a world is created based on the
@@ -33,22 +56,3 @@ SCENARIO("Loading and building a population from a valid file") {
   }
 }
 //---------------------------------------------------------------------------
-/**
- * @brief Test invalid fileName
- * @details In this scenarion the test is given an invalid filename. the
- * function should then correctly throw an exception and return an error message
- * to the user.
- */
-SCENARIO("Loading a population from an invalid file") {
-  GIVEN("a Map holding cells") {
-    map<Point, Cell> cells;
-    WHEN("an invalid filename is entered:") {
-      fileName = "invalid.txt";
-      FileLoader fileloader;
-      THEN("an exception should be thrown, with error message:") {
-        REQUIRE_THROWS_WITH(fileloader.loadPopulationFromFile(cells),
-                            "Could not find file. Closing application");
-      }
-    }
-  }
-}
