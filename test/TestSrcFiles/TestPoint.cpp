d726870d6fb343cf6e097aeb48f50c49827a49cf
johan <jkarlsson84@gmail.com>
2019-10-25 22:14:57 +0200

    Adding impementation of TestPoint functions

---
+++ test/TestSrcFiles/TestPoint.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Johan on 2019-10-25.
+//
+
+#include "TestPoint.h"
+#include "GoL_Rules/RuleOfExistence.h"
+
+Directions TestPoint::toDirections() { return Directions{x, y}; }
+
+TestPoint &TestPoint::operator+=(TestPoint const &obj) {
+    x += obj.x;
+    y += obj.y;
+    return *this;
+}
+
+TestPoint operator+(TestPoint lhs, TestPoint const &rhs) {
+    lhs += rhs;
+    return lhs;
+}
+
+std::ostream &operator<<(std::ostream &out, const TestPoint &f) {
+    out << f.x << "," << f.y;
+
+    return out;
+}
