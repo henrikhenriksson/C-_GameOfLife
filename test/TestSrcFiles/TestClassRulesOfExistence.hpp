ef6950f7fe8bb505c710255ff96f7368251f4606
johan <jkarlsson84@gmail.com>
2019-10-27 01:11:10 +0200

    Added testcase testAliveCellInAllDirections that test alice cells in expected directions Added check of getName() to constructor test Added documentation

--- test/TestSrcFiles/TestClassRulesOfExistence.hpp
+++ test/TestSrcFiles/TestClassRulesOfExistence.hpp
@@ -10,10 +10,10 @@
 #ifndef GAMEOFLIFE_TEST_TESTCLASSRULESOFEXISTENCE_HPP_
 #define GAMEOFLIFE_TEST_TESTCLASSRULESOFEXISTENCE_HPP_
 
+#import <algorithm>
 #import "GoL_Rules/RuleOfExistence.h"
 #import "AccessRulesOfExistence.hpp"
-
-
+#import "../TestUtil/TestUtil.hpp"
 
 class TestClassRulesOfExistence : public RuleOfExistence {
  private:
@@ -27,6 +27,13 @@ class TestClassRulesOfExistence : public RuleOfExistence {
       std::string expRuleName = DEFAULT_NAME
   );
 
+  /**
+   * @brief Test that the real directions matches the expected directions
+   * @details Test that the both directions is of the same length and that
+   * all elements match. For the catch test everyting is tested under THEN level
+   * @param realDir Real values that is to be checked
+   * @param expDir Expected values
+   */
   static void testCompareDirections(const vector<Directions> &realDir,
                                     const vector<Directions> &expDir);
 
@@ -38,17 +45,31 @@ class TestClassRulesOfExistence : public RuleOfExistence {
       std::string &expRuleName = DEFAULT_NAME
   );
 
+  /**
+   * @brief Tests that a alive cell att all positions is counted, if the
+   * position is among the positions set for the rule
+   *
+   * @details Tests the standard rule directions ALL_DIRECTIONS, CARDINAL and
+   * DIAGONAL
+   *
+   * @param testClass Instance to test
+   * @param center Position of the cell to test
+   * @param expDirections Directions where an alive cell is to be counted
+   */
+  static void testAliveCellInAllDirections(
+      AccessRulesOfExistence &testClass,
+      TestPoint &center,
+      const vector<Directions> expDirections);
+
 };
 
 #endif //GAMEOFLIFE_TEST_TESTCLASSRULESOFEXISTENCE_HPP_
-
 
 void TestClassRulesOfExistence::testValesRulesOfExistence(
     map<Point, Cell> &expCells,
     PopulationLimits expLimits,
     vector<Directions> expDirections,
-    std::string expRuleName )
-{
+    std::string expRuleName) {
     sTestValesRulesOfExistence(
         *testClass,
         expCells,
@@ -56,27 +77,33 @@ void TestClassRulesOfExistence::testValesRulesOfExistence(
         expDirections,
         expRuleName);
 }
-void TestClassRulesOfExistence::testCompareDirections(const vector<Directions> &realDir,
-                                                      const vector<Directions> &expDir) {
+
+void TestClassRulesOfExistence::testCompareDirections(
+    const vector<Directions> &readDir,
+    const vector<Directions> &expDir) {
+
     THEN("Vector sizes should match") {
-        REQUIRE(realDir.size() == expDir.size());
+        //Check that both vectors has the same length. Not placed in a THEN
+        // block since it does not make since to continue if the sizes differ
+        REQUIRE(readDir.size() == expDir.size());
         WHEN("Comparing elements") {
-            auto realIt = realDir.begin();
-            auto expIt = expDir.begin();
-            int i = 0;
-            while (realIt != realDir.end()) {
-                stringstream header;
-                header << "Value at pos: " << i << "should match";
-                THEN(header.str()) {
-                    REQUIRE(realIt->HORIZONTAL == expIt->HORIZONTAL);
-                    REQUIRE(realIt->VERTICAL == expIt->VERTICAL);
+
+            //Iterate the vectors and compare each element.
+            //It would be better to use
+            // std::equal(expDir.begin(), expDir.en(), vecOfNums2.begin());
+            // but it is not possible since DIRECTIONS has implemented the
+            // needed operators (==)
+            THEN("All elements should match") {
+                auto expIt = expDir.begin();
+                auto readIt = readDir.begin();
+                for (; expIt != expDir.end(); readIt++, expIt++) {
+                    //Compare DIRECTIONS elements
+                    CHECK(readIt->HORIZONTAL == expIt->HORIZONTAL);
+                    CHECK(readIt->VERTICAL == expIt->VERTICAL);
                 }
-                realIt++;
-                expIt++;
             }
         }
     }
-
 }
 
 void TestClassRulesOfExistence::sTestValesRulesOfExistence(
@@ -88,9 +115,14 @@ void TestClassRulesOfExistence::sTestValesRulesOfExistence(
     std::stringstream header;
 
     //Check that the rule name is set
-    header << "The correct name should be used: " << DEFAULT_NAME;
+    header << "The correct name should be used: " << expRuleName;
     THEN(header.str()) {
         REQUIRE(testClass.ruleName == expRuleName);
+    }
+
+        //Check that getRuleName() returns the expected value
+    THEN("getRuleName() should return the correct name") {
+        REQUIRE(testClass.getRuleName() == expRuleName);
     }
 
     //Check that the Population limits is set
@@ -112,5 +144,70 @@ void TestClassRulesOfExistence::sTestValesRulesOfExistence(
 
 }
 
+void TestClassRulesOfExistence::testAliveCellInAllDirections(
+    AccessRulesOfExistence &testClass,
+    TestPoint &center,
+    const vector<Directions> expDirections) {
+
+    //Use age of two to test that older cells is only counted once
+    int aliveCellAge = 2;
+
+    //Check that a alive cell is detected when placed in all directions
+    for (auto aliveDirection : ALL_DIRECTIONS) {
+
+        //Calculate the position of the alive cell
+        TestPoint aliveAbsPos = center + TestPoint(aliveDirection);
+
+        int expAliveCount = -1;
+
+        //Check if current direction is expected to be checked
+        auto cell_found =
+            std::find_if(expDirections.begin(), expDirections.end(),
+                         [&aliveDirection](const Directions &d1) {
+                           return (
+                               (aliveDirection.VERTICAL == d1.VERTICAL)
+                                   && (aliveDirection.HORIZONTAL
+                                       == d1.HORIZONTAL)
+                           );
+                         });
+
+        //set expAlive to 1 counter if the current direction is checked
+        if (cell_found != expDirections.end()) {
+            //Cell should be counted
+            expAliveCount = 1;
+        } else {
+            //Cell should not be counted
+            expAliveCount = 0;
+        }
+
+        //Create test header
+        std::stringstream header;
+        header << "Am alive cell at direction: "
+               << TestPoint(aliveDirection)
+               << ", abs pos: " << aliveAbsPos;
+
+        WHEN(header.str()) {
+
+            //Update the "alive" cell
+            TestUtil::updateCellAge(testClass.cells,
+                                    aliveAbsPos,
+                                    aliveCellAge);
+
+            header.str(std::string(""));
+            header << "countAliveNeighbours() should return: " <<
+                   expAliveCount;
+
+            THEN(header.str()) {
+
+                REQUIRE(
+                    expAliveCount ==
+                        testClass.countAliveNeighbours(center.toPoint())
+                );
+            }
+
+        }
+    }
+
+}
 
 
