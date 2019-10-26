063cff13aa11036312108f31f0cb4e23105dff5e
johan <jkarlsson84@gmail.com>
2019-10-27 01:12:14 +0200

    Added testcases

--- test/test-RulesOfExistence.cpp
+++ test/test-RulesOfExistence.cpp
@@ -11,86 +11,210 @@
 
 #define RULES_OF_EXISTENCE_TAG "[RulesOfExistence]"
 
+/**
+ * @brief Test that the Constructor sets and stores the expected values
+ */
 SCENARIO("Set constructor values should be stored in class",
          RULES_OF_EXISTENCE_TAG) {
     GIVEN("Default constuctor used") {
         map<Point, Cell> cells;
 
+        TestUtil::createMap(cells, 10, 10, false);
+
+        std::cout << "Map size2: " << cells.size() << std::endl;
+
         AccessRulesOfExistence testClass(cells);
 
         TestClassRulesOfExistence::sTestValesRulesOfExistence(testClass, cells);
+    }GIVEN("Custom values sent to the Constuctor") {
+        map<Point, Cell> cells;
+
+        TestUtil::createMap(cells, 10, 10, false);
+
+        std::cout << "Map size2: " << cells.size() << std::endl;
+
+        PopulationLimits expLim{1, 2, 3};
+
+        std::string expName = "Kalle";
+
+        AccessRulesOfExistence testClass(cells, DIAGONAL, expLim, expName);
+
+        TestClassRulesOfExistence::sTestValesRulesOfExistence(testClass,
+                                                              cells,
+                                                              expLim,
+                                                              DIAGONAL,
+                                                              expName);
     }
 }
 
-SCENARIO("Given a existing board", RULES_OF_EXISTENCE_TAG) {
-    //Test for rules using the 3 defined direction types
-    //All, Cardinal, Diagonal
+/**
+ * @brief Test that all an alive cell is detected at all available positions
+ * when ALL_DIRECTIONS is used
+ *
+ * @details Uses a minimized game board of size [1,1] which will create a
+ * board of absolute size [3,3] since this contains the tested cell and
+ * sorrounding cells.
+ * Also tests that cells older than 1 iteratin only is tested once
+ */
+SCENARIO("Alive cells should be detected in all directions",
+         RULES_OF_EXISTENCE_TAG) {
 
-    //Create board
+    //Create minimal board that defines sorounding cells for the tested cell,
+    // center
     map<Point, Cell> cells;
     TestUtil::createMap(cells, 1, 1, false);
 
-    //Check start conditions
-    GIVEN("An new game board") {
+    //define center point to set new values
+    TestPoint center{1, 1};
 
-        WHEN("Elements are added") {
+    GIVEN("Rules That checks ALL_DIRECTIONS") {
+        //Create a test instance
+        AccessRulesOfExistence testClass(cells, ALL_DIRECTIONS);
 
-            THEN("The bord should not be empty") {
-                REQUIRE(!cells.empty());
-                REQUIRE(9 == cells.size());
-            }
-        }
+        TestClassRulesOfExistence::testAliveCellInAllDirections(
+            testClass,
+            center,
+            ALL_DIRECTIONS);
     }
 
-    //Create a test instance
-    AccessRulesOfExistence testClass(cells, ALL_DIRECTIONS);
+    GIVEN("Rules That checks CARDINAL directions") {
+        // header
+        //Create a test instance
+        AccessRulesOfExistence testClass(cells, CARDINAL);
+
+        TestClassRulesOfExistence::testAliveCellInAllDirections(
+            testClass,
+            center,
+            CARDINAL);
+    }
+
+    GIVEN("Rules That checks DIAGONAL directions") {
+        // header
+        //Create a test instance
+        AccessRulesOfExistence testClass(cells, DIAGONAL);
+
+        TestClassRulesOfExistence::testAliveCellInAllDirections(
+            testClass,
+            center,
+            DIAGONAL);
+    }
+}
+
+/**
+ * @brief Test that countAliveNeighbours returns the correct nr of alive
+ * neighbours.
+ *
+ * @details Test for 0-9 alive cells.
+ */
+SCENARIO("Should be able to count all alive cells", RULES_OF_EXISTENCE_TAG) {
+    //Create minimal board that defines sorounding cells for the tested cell,
+    // center
+    map<Point, Cell> cells;
+    TestUtil::createMap(cells, 1, 1, false);
 
     //define center point to set new values
     TestPoint center{1, 1};
-    GIVEN("Rules That checks all directions") { ///@toto add direction to header
 
+    GIVEN("Rules that checks all directions") {
+        AccessRulesOfExistence testClass(cells);
 
-        int age = 1;
-
-        for (auto alivePos : ALL_DIRECTIONS) {
-
-            TestPoint pointAlive = center + TestPoint(alivePos);
-
-            //Test age to use
+        for (int nrAliveCells = 0; nrAliveCells <= ALL_DIRECTIONS.size();
+             nrAliveCells++) {
             std::stringstream header;
-            header << "Current Alive cell is placed at distance from Cell: " ;
-//                      "" << alivePos << " abs pos: " << pointAlive;
-
+            header << nrAliveCells << " nr of neighbors is set to alive";
             WHEN(header.str()) {
-                //Update the current direction from the center point [1,1]
-                std::vector<pair<TestPoint, int>> newCellAges;
-                newCellAges.emplace_back(alivePos, age);
 
-
-                //Update cell values
-                TestUtil::updateCellAge(cells, center.toPoint(), newCellAges);
-
-                bool cellIsAlive;
-
-//                std::cout << "Cell" << alivePos << std::endl;
-
-                cellIsAlive = cells.at(
-                    pointAlive.toPoint()).isAlive();
-
-                THEN("The point ... should be alive") { //Requiere update header
-                    REQUIRE(cellIsAlive);
+                //Set alive cells
+                auto it = ALL_DIRECTIONS.begin();
+                for (int i = 0; i < nrAliveCells; i++, it++) {
+                    TestUtil::updateCellAge(cells, TestPoint(*it) + center, 1);
                 }
 
-                THEN("Alive counter for the center point should return 1") {
-                    // Why is direction no longer set in testClass
-                    TestUtil::printCellAge(cells);
-                    REQUIRE(
-                        1 == testClass.countAliveNeighbours(center.toPoint())
-                    );
+                //Check that the correct nr of alive cells is returned
+                header.str(std::string(""));
+                header << "countAliveNeighbours should return: " <<
+                       nrAliveCells;
+                THEN(header.str()) {
+                    REQUIRE(nrAliveCells
+                                == testClass.countAliveNeighbours(center.toPoint()));
                 }
             }
         }
     }
-
 }
 
+SCENARIO("Should handle if Points outside the board passed") {
+    //Create minimal board that defines sorounding cells for the tested cell,
+    // center
+    map<Point, Cell> cells;
+    TestUtil::createMap(cells, 1, 1, false);
+
+    //define center point to set new values
+    GIVEN("A game board of size [3,3]") {
+        AccessRulesOfExistence testClass(cells);
+
+        WHEN("A valid point is given[3,2]") {
+            TestPoint invalidPoint(1, 1);
+            THEN("No error should be thrown") {
+                REQUIRE_NOTHROW(
+                    testClass.countAliveNeighbours(invalidPoint.toPoint())
+                );
+            }
+        }
+
+        WHEN("A rim cell is given [0,1]") {
+            TestPoint invalidPoint(0, 1);
+            THEN("No error should be thrown") {
+                REQUIRE_NOTHROW(
+                    testClass.countAliveNeighbours(invalidPoint.toPoint())
+                );
+            }
+        }
+
+        WHEN("A rim cell is given [2,1]") {
+            TestPoint invalidPoint{2, 1};
+            THEN("No error should be thrown") {
+                REQUIRE_NOTHROW(
+                    testClass.countAliveNeighbours(invalidPoint.toPoint())
+                );
+            }
+        }
+
+        WHEN("A rim cell is given [1,0]") {
+            TestPoint invalidPoint(1, 0);
+            THEN("No error should be thrown") {
+                REQUIRE_NOTHROW(
+                    testClass.countAliveNeighbours(invalidPoint.toPoint())
+                );
+            }
+        }
+
+        WHEN("A rim cell is given [1,2]") {
+            TestPoint invalidPoint(1, 2);
+            THEN("No error should be thrown") {
+                REQUIRE_NOTHROW(
+                    testClass.countAliveNeighbours(invalidPoint.toPoint())
+                );
+            }
+        }
+
+        WHEN("A negative value cells is given [-1,2]") {
+            TestPoint invalidPoint(-1, 2);
+            THEN("No error should be thrown") {
+                REQUIRE_NOTHROW(
+                    testClass.countAliveNeighbours(invalidPoint.toPoint())
+                );
+            }
+        }
+
+        WHEN("A negative value cells is given [1,-2]") {
+            TestPoint invalidPoint(1, -2);
+            THEN("No error should be thrown") {
+                REQUIRE_NOTHROW(
+                    testClass.countAliveNeighbours(invalidPoint.toPoint())
+                );
+            }
+        }
+
+    }
+}
