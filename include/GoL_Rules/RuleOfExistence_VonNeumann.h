e604eeb59315e9480e5e1f278ff9c114c0716044
johan <jkarlsson84@gmail.com>
2019-10-29 14:32:11 +0100

    Updated doxygen comments

--- include/GoL_Rules/RuleOfExistence_VonNeumann.h
+++ include/GoL_Rules/RuleOfExistence_VonNeumann.h
@@ -1,29 +1,49 @@
-/*
- * Filename    RuleOfExistence_VonNeumann.h
- * Author      Erik Ström
- * Date        July 2019
- * Version     0.2
+/**
+ * @file    RuleOfExistence_VonNeumann.h
+ * @author      Erik Ström
+ * @date        July 2019
+ * @version     0.2
+ *
+ * @brief This class contains the definitions of the VonNeumann Game of life
+ * rules
 */
 
 #ifndef GAMEOFLIFE_RULEOFEXISTENCE_VONNEUMANN_H
 #define GAMEOFLIFE_RULEOFEXISTENCE_VONNEUMANN_H
 
 #include "RuleOfExistence.h"
-/*
-Von Neumann's RuleOfExistence, differs from Conway in that only 4 neighbours are accounted for.
-
-Concrete Rule of existence, implementing Von Neumann's rule.
-Only difference from Conway is that neighbours are determined using only cardinal directions (N, E, S, W).
+/**
+ * @brief Von Neumann's RuleOfExistence, differs from Conway in that only 4
+ * neighbours are accounted for.
+ *
+ * @details Concrete Rule of existence, implementing Von Neumann's rule.
+ * Only difference from Conway is that neighbours are determined using only
+ * cardinal directions (N, E, S, W).
 */
 class RuleOfExistence_VonNeumann : public RuleOfExistence
 {
 private:
 
 public:
+  /// @brief Default constructor
     explicit RuleOfExistence_VonNeumann(map<Point, Cell>& cells)
             : RuleOfExistence({ 2,3,3 }, cells, CARDINAL, "von_neumann") {}
-    ~RuleOfExistence_VonNeumann() override = default;
+    /// @brief Default destructor
+            ~RuleOfExistence_VonNeumann() override = default;
 
+/**
+ * @brief calculates the next generation values for the VonNewmann rules
+ *
+ * @details Uses Cardinal directions when checking for alive neighbours
+ * Sets the color STATE_COLORS.DEAD to cells that are killed
+ * Sets the color STATE_COLORS.LIVING to cells that are given life
+ * Uses default functions for counting alive neighbours and which action to take
+ *
+ * Test Recommendations
+ * Test that only alice cells at cardinal directions is counted
+ * Test that the correct color is set for cells that are killed
+ * Test that the correct color is set for cells that are given life
+ */
     void executeRule() override;
 };
 
