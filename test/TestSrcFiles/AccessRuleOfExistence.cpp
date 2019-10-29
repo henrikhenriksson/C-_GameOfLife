b8622e3aefccaa85456448dacb630b7af1fc95dc
johan <jkarlsson84@gmail.com>
2019-10-29 12:22:34 +0100

    Renamed file to match parrent class file Added comments Updated include name after name change

--- test/TestSrcFiles/AccessRuleOfExistence.cpp
+++ test/TestSrcFiles/AccessRuleOfExistence.cpp
@@ -1,14 +1,16 @@
 //
 // Created by Johan on 2019-10-28.
+// Doxygen comments not made in .cpp files
+// Conintains implementation for access files for AccessRulesOfExistence
 //
 
-#include "AccessRulesOfExistence.h"
+#include "AccessRuleOfExistence.h"
 
 
 
 int AccessRulesOfExistence::countAliveNeighbours(Point center)
 {
-return RuleOfExistence::countAliveNeighbours(center);
+    return RuleOfExistence::countAliveNeighbours(center);
 }
 
 ACTION AccessRulesOfExistence::getAction(int aliveNeighbours,
