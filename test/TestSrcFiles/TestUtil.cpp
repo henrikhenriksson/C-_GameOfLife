b6d442681a255bd719ed1c907e70648457f21c82
johan <jkarlsson84@gmail.com>
2019-10-29 12:31:49 +0100

    Added comments. Using TestPoint for a more compact print

--- test/TestSrcFiles/TestUtil.cpp
+++ test/TestSrcFiles/TestUtil.cpp
@@ -1,10 +1,15 @@
 //
 // Created by Johan on 2019-10-28.
+// Doxygen comments not used in .cpp files
 //
 
 #include "TestUtil.h"
 
 //-------------------------------------------------------------------------------------
+/*
+ * Creates a game board of wanted dimensions. Caller decides if rim cells is
+ * to be set or not
+ */
 void TestUtil::createMap(std::map<Point, Cell> &cells,
                int height,
                int width,
@@ -18,6 +23,8 @@ void TestUtil::createMap(std::map<Point, Cell> &cells,
     //Iterate through the world and create a new cell for each position
     for (int row = 0; row <= height + 1; row++) {
         for (int col = 0; col <= width + 1; col++) {
+            //Create a Point and cell with the wanted position
+            //If the cell is suposed to be a rim cell the rim cell status is set
             cells.insert(
                 pair<Point, Cell>
                     (
@@ -30,28 +37,41 @@ void TestUtil::createMap(std::map<Point, Cell> &cells,
     }
 }
 
+/*
+ * Prints the rimcell and its status to shell to cout
+ */
 void TestUtil::printIsRimCell(std::map<Point, Cell> &cells) {
     //Iterate through all the cells and print their rim cell status
     for (auto mapIt : cells) {
-        std::cout << mapIt.first.x << "," << mapIt.first.y
+        std::cout << TestPoint(mapIt.first)
                   << ":" << mapIt.second.isRimCell() << std::endl;
     }
 }
 
+/*
+ * Check if a cell at the given position should be a rim cell or not
+ */
 bool TestUtil::isPosRimCell(int row, int col) {
     //Check if position is around the boarders of the game board
     return ((row == 0) || (row == WORLD_DIMENSIONS.HEIGHT + 1)
         || (col == 0) || col == WORLD_DIMENSIONS.WIDTH + 1);
 }
 
+/*
+ * Prints the cell position and age to cout
+ */
 void TestUtil::printCellAge(std::map<Point, Cell> &cells) {
     //Iterate through all the cells and print position and age
     for (auto mapIt : cells) {
-        std::cout << " " << mapIt.first.x << "," << mapIt.first.y
-                  << ":" << mapIt.second.getAge() << std::endl;
+        std::cout << " " << TestPoint(mapIt.first)
+        << ":" << mapIt.second.getAge() << std::endl;
     }
 }
 
+/*
+ * Updates cell ages for the given cells. Cells is given with a direction
+ * from the center point given
+ */
 void TestUtil::updateCellAge(std::map<Point, Cell> &cells,
                    TestPoint center,
                    std::vector<pair<TestPoint, int>> &newCellAges) {
@@ -63,10 +83,12 @@ void TestUtil::updateCellAge(std::map<Point, Cell> &cells,
         TestUtilCell::setCellAge(
             cells[(center + update.first).toPoint()],
             update.second);
-        //TestUtilCell::setCellAge(cells[newPoint], update.second);
     }
 }
 
+/*
+ * Updates the cell age for a given position
+ */
 void TestUtil::updateCellAge(std::map<Point, Cell> &cells,
     TestPoint pos,
     int age)
