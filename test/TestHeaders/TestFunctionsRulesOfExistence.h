aedfe9e3e0314f08b3e5d3563e02cbae748bf57c
johan <jkarlsson84@gmail.com>
2019-10-29 12:20:15 +0100

    Updated doxygen comments

--- test/TestHeaders/TestFunctionsRulesOfExistence.h
+++ test/TestHeaders/TestFunctionsRulesOfExistence.h
@@ -3,7 +3,8 @@
  * @author Johan Karlsson, joka1806
  *
  * @brief This file contains test functionality for the RuleOfExistence class
- *
+ * @details Some common used test cases has been extracted to this file for
+ * multiple use. This so the test file can contain only SCENARIOS or TestCases
  */
 
 #ifndef GAMEOFLIFE_TEST_TESTCLASSRULESOFEXISTENCE_HPP_
@@ -12,10 +13,14 @@
 #include <map>
 #include <algorithm>
 #include "catch.hpp"
-#include "GoL_Rules/RuleOfExistence.h"
-#include "AccessRulesOfExistence.h"
+#include "AccessRuleOfExistence.h"
 #include "TestUtil.h"
+#include "TestPoint.h"
+#include "GoL_Rules/RuleOfExistence.h"
 
+/**
+ * @brief Namespace is used to separate test areas
+ */
 namespace TestFunctionsRuleOfExistence {
 
 /**
@@ -28,6 +33,17 @@ namespace TestFunctionsRuleOfExistence {
 static void testCompareDirections(const vector<Directions> &realDir,
                                   const vector<Directions> &expDir);
 
+
+/**
+ * @brief Test the the expected values has been set in the class
+ * @details Test on THEN level, Expects GIVEN and WHEN to be set by calling
+ * functions
+ * @param testClass Instance to test
+ * @param expCells Expected cells, only size are tested for now
+ * @param expLimits Expected limits
+ * @param expDirections Expected directions
+ * @param expRuleName Expected rule name
+ */
 void testValesRulesOfExistence(
     AccessRulesOfExistence &testClass,
     map<Point, Cell> &expCells,
@@ -37,11 +53,14 @@ void testValesRulesOfExistence(
 );
 
 /**
- * @brief Tests that a alive cell att all positions is counted, if the
- * position is among the positions set for the rule
+ * @brief Tests that a alive cell att all positions is counted.
  *
- * @details Tests the standard rule directions ALL_DIRECTIONS, CARDINAL and
- * DIAGONAL
+ * @details The directions to test is specified by expDirections. This should
+ * be the same as the directions by the tested rule.
+ * Test is performed so a cell is set to alive at a direction at a time. All
+ * Directions is tested and if the current direction is among the expected
+ * direction the cell is supposed to be counted. If the direction is not among
+ * the expected directions the cell is not to be counted.
  *
  * @param testClass Instance to test
  * @param center Position of the cell to test
@@ -53,9 +72,13 @@ void testAliveCellInAllDirections(
     vector<Directions> expDirections);
 
 /**
- * @brief Returns the action for a cell with the given nr of alive neighbours
+ * @brief Returns the expected action for a cell with a given nr of alive
+ * neighbours
  *
- * @param aliveNeighbours
+ * @details This function returns DO_NOTHING if an alive count larger than 9
+ * is returned.
+ *
+ * @param aliveNeighbours Alive cell count
  * @param aliveCell Is the cell current alive or dead
  * @return
  */
